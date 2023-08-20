8efe27766eb9ecdfaf2e00249f3a007d9f2abe32
otterslacks <antonelloluke@gmail.com>
2023-08-20 19:34:42 -0400

    more edits to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,27 +1,25 @@
-const router = require('express').Router();
-const { Project, User } = require('../models');
-const withAuth = require('../utils/auth');
-const raceTheTurtle = require('../utils/randomRaceTurtle');
+const router = require("express").Router();
+const { Post, Blogger, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-// Home page route
-router.get('/', async (req, res) => {
+// Render homepage with recent posts from all users
+router.get("/", async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    // Retrieve all posts including associated blogger data
+    const postData = await Post.findAll({
       include: [
         {
-          model: User,
-          attributes: ['name'],
+          model: Blogger,
+          attributes: ["username"],
         },
       ],
     });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Render homepage template with projects data and login status
-    res.render('homepage', {
-      projects,
+    // Render the homepage template with posts data and login status
+    res.render("homepage", {
+      posts,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -29,47 +27,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Specific project route
-router.get('/project/:id', async (req, res) => {
+// Render dashboard if logged in
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
-    // Find a specific project by ID and include user data
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    // Retrieve logged-in blogger data including their posts
+    const bloggerData = await Blogger.findByPk(req.session.blogger_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
     });
 
-    // Serialize project data for rendering
-    const project = projectData.get({ plain: true });
+    const blogger = bloggerData.get({ plain: true });
 
-    // Render project template with project data and login status
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Profile route (requires authentication)
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged-in user's data including associated projects
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    // Serialize user data for rendering
-    const user = userData.get({ plain: true });
-
-    // Render profile template with user data and login status
-    res.render('profile', {
-      ...user,
+    // Render the dashboard template with blogger data and login status
+    res.render("dashboard", {
+      ...blogger,
       logged_in: true,
     });
   } catch (err) {
@@ -77,22 +48,84 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-// Login page route
-router.get('/login', (req, res) => {
-  // Redirect to profile if already logged in
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+// Render form for new post
+router.get("/new-post", withAuth, async (req, res) => {
+  try {
+    // Retrieve logged-in blogger data including their posts
+    const bloggerData = await Blogger.findByPk(req.session.blogger_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
 
-  // Render login template
-  res.render('login');
+    const blogger = bloggerData.get({ plain: true });
+
+    // Render the new-post template with blogger data and login status
+    res.render("new-post", {
+      ...blogger,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Race the Turtle route
-router.get('/race-turtle', raceTheTurtle, (req, res) => {
-  // Render turtle template with race data
-  res.render("turtle", req.turtle);
+// Render individual post route with comments
+router.get("/post/:id", withAuth, async (req, res) => {
+  try {
+    // Retrieve specific post data including associated comments and bloggers
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [Blogger],
+        },
+        {
+          model: Blogger,
+          attributes: ["username", "id"],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    // Determine which template to render based on the logged-in blogger's ownership
+    const renderTemplate = post.blogger_id === req.session.blogger_id ? "post" : "comment";
+
+    // Render the chosen template with post data, blogger info, and login status
+    res.render(renderTemplate, {
+      ...post,
+      blogger_id: req.session.blogger_id,
+      blogger_name: req.session.blogger_name,
+      commenter: req.session.commenter,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Render login route
+router.get("/login", (req, res) => {
+  try {
+    // Redirect to the dashboard if already logged in, otherwise render login template
+    if (req.session.logged_in) {
+      res.redirect("./dashboard");
+      return;
+    }
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Render signup route
+router.get("/signup", (req, res) => {
+  try {
+    // Render the signup template
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
