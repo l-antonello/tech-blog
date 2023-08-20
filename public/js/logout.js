084f107d5354e947af4b8723b4c5a813b24cc675
otterslacks <antonelloluke@gmail.com>
2023-08-20 19:46:32 -0400

    edits to login

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,21 @@
 const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+  try {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
+    if (response.ok) {
+      // Redirect to the homepage after successful logout
+      document.location.replace('/');
+    } else {
+      // Display an error message if logout fails
+      alert('Logout failed. Please try again.');
+    }
+  } catch (err) {
+    console.error('An error occurred:', err);
   }
 };
 
+// Attach the logout function to the click event of the logout button
 document.querySelector('#logout').addEventListener('click', logout);
